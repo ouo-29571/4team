3a1c10351bc6e0a821ff9a695a9464a54c8cafd4
CL5534 <cof5534@gmail.com>
2025-06-11 16:49:18 +0900

    스타  평균 적용 준비
    스타 중

--- front/src/Component/Menu/Product/Star.jsx
+++ front/src/Component/Menu/Product/Star.jsx
@@ -1,28 +1,48 @@
 import { useState } from 'react';
 
-const Star = () => {
-    const [nowClicked, setNowClicked] = useState(0); // 클릭된 별 번호 저장
-    const handleClick = (num) => {
-        setNowClicked(num);
-        alert(`${num}점 감사합니다다.`);
-    };
+const Star = ({ productId }) => {
+  const [nowClicked, setNowClicked] = useState(0); // 클릭된 별 번호 저장
+  
+  // ⭐️ POST 요청 보내서 product_ratings 테이블에 INSERT
+  const handleClick = async (num) => {
+    setNowClicked(num);
+    alert(`${num}점 감사합니다.`);
 
-    return (
-        <div>
-            <div className="inner_left_Star">  {/* 별이 다섯개 */}
-                <div style={{ fontSize: "20px", cursor: "pointer", color: "black" }}>
-                    {[1, 2, 3, 4, 5].map((num) => (
-                        <span
-                            key={num}
-                            onClick={() => handleClick(num)}
-                            style={{ color: num <= nowClicked ? "black" : "gray" }} >
-                            {num <= nowClicked ? "★" : "☆"}
-                        </span>
-                    ))}
-                </div>
-            </div>
+    try {
+      const res = await fetch(
+        `http://localhost:8080/api/products/${productId}/rating`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ rating: num }),
+        }
+      );
+      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      const data = await res.json();
+      console.log("별점 등록 성공:", data);
+    } catch (error) {
+      console.error("별점 등록 실패:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="inner_left_Star">
+        <div style={{ fontSize: "20px", cursor: "pointer", color: "black" }}>
+          {[1, 2, 3, 4, 5].map((num) => (
+            <span
+              key={num}
+              onClick={() => handleClick(num)}
+              style={{ color: num <= nowClicked ? "black" : "gray" }}>
+              {num <= nowClicked ? "★" : "☆"}
+            </span>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Star;
