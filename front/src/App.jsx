326d9cab06528d9631399794e8538580138bc00c
CL5534 <cof5534@gmail.com>
2025-06-13 12:39:55 +0900

    고객센터 이용약관
    ㅎ

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -22,37 +22,35 @@ import Cart from "./Component/cart/Cart";
 import Order from "./Component/order/Order";
 import History from "./Component/history/History";
 import CustomerCenter from "./Component/Menu/Footer/CustomerCenter/CustomerCenter";
-
+import TermsOfService from "./Component/Menu/Footer/TermsOfService/TermsOfService";
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    {/* <Route path="/" element={<Main />} /> */}
-                    <Route path="/" element={<Main />} />
-                    <Route path="/Menu" element={<Menu />} />
-                    <Route path="/DetailPage/:id" element={<DetailPage />} />
-                    <Route path="/Login" element={<Login />} />
-                    <Route path="/Signup" element={<Signup />} />
-                    <Route path="/Passwordfind" element={<Passwordfind />} />
-                    <Route path="/Passwordfix" element={<Passwordfix />} />
-                    <Route path="/Mypage" element={<Mypage />} />
-                    <Route path="/Userinfofix" element={<Userinfofix />} />
-                    <Route path="/Cart" element={<Cart />} />
-                    <Route path="/EventPage" element={<EventPage />} />
-                    <Route path="/ImageSlider" element={<ImageSlider />} />
-                    <Route path="/Order" element={<Order />} />
-                    <Route path="/History" element={<History />} />
-                    <Route
-                        path="/CustomerCenter"
-                        element={<CustomerCenter />}
-                    />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Main />} /> */}
+          <Route path="/" element={<Main />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/DetailPage/:id" element={<DetailPage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Passwordfind" element={<Passwordfind />} />
+          <Route path="/Passwordfix" element={<Passwordfix />} />
+          <Route path="/Mypage" element={<Mypage />} />
+          <Route path="/Userinfofix" element={<Userinfofix />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/EventPage" element={<EventPage />} />
+          <Route path="/ImageSlider" element={<ImageSlider />} />
+          <Route path="/Order" element={<Order />} />
+          <Route path="/History" element={<History />} />
+          <Route path="/CustomerCenter" element={<CustomerCenter />} />
+          <Route path="/TermsOfService" element={<TermsOfService />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
