cb2c7c222cbb79266212865ac71c7231ec3ed472
jun-beom2 <junbeom157@gmail.com>
2025-06-17 17:13:18 +0900

    ddd

--- front/src/Component/CommaNumber.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { formatNumber } from '@/utils/formatNumber'
-
-export default function CommaNumber({ value }) {
-  return <>{formatNumber(value)}</>
-}
--- front/src/utils/formatNumber.js
+++
@@ -1,4 +0,0 @@
-export function formatNumber(num, locale = 'ko-KR') {
-  if (typeof num !== 'number') return num;
-  return num.toLocaleString(locale);
-}
